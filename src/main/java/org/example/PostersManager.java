023b9d93c2f82259d129d2c276ec460cc10cfe3c
neto study <kseniyalazare@yandex.ru>
2023-03-13 19:29:04 +0300

    initial commit

---
+++ src/main/java/org/example/PostersManager.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class PostersManager {
+    private Posters[] movies = new Posters[0];
+    private int last;
+
+    public PostersManager() {this.last = 5;}
+    public PostersManager(int last) {this.last=last;}
+
+    public void save(Posters movie) {
+        Posters[] tmp = new Posters[movies.length +1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+
+    }
+    public Posters[] getMovies() {
+        return movies;
+    }
+
+    public Posters[] findAll() {
+        Posters[] movies = getMovies(); {
+        return movies;
+        }
+    }
+    public Posters[]  findLast() {
+        int resultLength;
+        if (movies.length >= last) {
+           resultLength = last;
+        } else {
+            resultLength = movies.length;
+        }
+
+        Posters[] result = new Posters[resultLength];
+        for (int i = resultLength -1; i >= 0; i--) {
+            result[resultLength - 1 - i] = movies[i];
+        }
+        return result;
+    }
+}
---
+++ src/test/java/FindAllTest.java
@@ -0,0 +1,39 @@
+import org.example.Posters;
+import org.example.PostersManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FindAllTest {
+
+    PostersManager pManager = new PostersManager(4);
+
+    Posters movie1 = new Posters("Джентельмены");
+    Posters movie2 = new Posters("Вперед");
+    Posters movie3 = new Posters("Бладшот");
+    Posters movie4 = new Posters("Тролли");
+
+    @BeforeEach
+    public void setup() {
+        pManager.save(movie1);
+        pManager.save(movie2);
+        pManager.save(movie3);
+        pManager.save(movie4);
+    }
+
+    @Test
+    public void findLastLessTest() {
+
+
+        Posters[] expected = {movie4, movie3, movie2, movie1};
+        Posters[] actual = pManager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+
+
+
+}
